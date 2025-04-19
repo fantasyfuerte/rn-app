96475fc035e442f38c9a509290680b640734886b
Fuerte <leandrofuerte75@gmail.com>
2025-04-19 19:48:56 -0400

    quitting extra imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/ui/header";
 import SwitchScreen from "@/components/ui/switch-screen";
-import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
