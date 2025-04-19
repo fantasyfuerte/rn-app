10aa79b98a18c9b4a119e5df596bc1c8e40c4a16
Fuerte <leandrofuerte75@gmail.com>
2025-04-19 16:03:34 -0400

    Refactor HomeScreen component to enhance structure and styling, adding header with search and filter buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,53 @@
-import { Image, StyleSheet, Platform } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { FontAwesome } from "@expo/vector-icons";
+import { useTheme } from "@react-navigation/native";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return <SafeAreaView></SafeAreaView>;
+  const theme = useTheme();
+  console.log(theme);
+
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>WorkersHub</Text>
+        <View style={styles.headerButtons}>
+          <TouchableOpacity>
+            <FontAwesome name="search" size={20} color={"#fff"} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <FontAwesome name="filter" size={20} color={"#fff"} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 16,
+  },
+  headerText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  headerButtons: {
+    display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+    gap: 20,
   },
 });
