0f312b990ae43f26a576da63f2fa61cc77c1e6f9
Fuerte <leandrofuerte75@gmail.com>
2025-04-19 19:37:52 -0400

    Componentizing header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/ui/header";
 import SwitchScreen from "@/components/ui/switch-screen";
 import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
@@ -9,40 +10,10 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>WorkersHub</Text>
-        <View style={styles.headerButtons}>
-          <TouchableOpacity>
-            <FontAwesome name="search" size={20} color={"#fff"} />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome name="filter" size={20} color={"#fff"} />
-          </TouchableOpacity>
-        </View>
-      </View>
+      <Header />
       <SwitchScreen selected={selected} setSelected={setSelected} />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 16,
-    paddingVertical: 24,
-  },
-  headerText: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "800",
-  },
-  headerButtons: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 20,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/ui/header.tsx
@@ -0,0 +1,40 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>WorkersHub</Text>
+      <View style={styles.headerButtons}>
+        <TouchableOpacity>
+          <FontAwesome name="search" size={20} color={"#fff"} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome name="filter" size={20} color={"#fff"} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 16,
+    paddingVertical: 24,
+  },
+  headerText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "800",
+  },
+  headerButtons: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+});
