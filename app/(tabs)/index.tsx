b5c1f046e7e67bdbeef4b6f749c3a2f80400782d
Fuerte <leandrofuerte75@gmail.com>
2025-04-19 16:38:07 -0400

    Remove unused imports in HomeScreen component for cleaner code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,7 @@
 import Switchscreen from "@/components/ui/switch-screen";
 import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
-import {
-  Image,
-  StyleSheet,
-  Platform,
-  View,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
