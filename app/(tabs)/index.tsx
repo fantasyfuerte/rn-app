fd6f03f2e0fd4b4a3dd76729e4a807aa58b14e2f
Fuerte <leandrofuerte75@gmail.com>
2025-04-19 20:33:55 -0400

    exporting styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
+import AvailableJobs from "@/components/container/available-jobs";
 import Header from "@/components/ui/header";
 import SwitchScreen from "@/components/ui/switch-screen";
 import { useState } from "react";
-import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   const [selected, setSelected] = useState<"available-jobs" | "my-jobs">(
@@ -13,8 +14,16 @@ export default function HomeScreen() {
     <SafeAreaView>
       <Header />
       <SwitchScreen selected={selected} setSelected={setSelected} />
+      {selected == "available-jobs" && <AvailableJobs />}
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "700",
+    margin: 24,
+    color: "#fff",
+  },
+});
