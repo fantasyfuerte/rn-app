550c57d48d6c3477c8a854bfce1f8488855d76cc
Fuerte <leandrofuerte75@gmail.com>
2025-04-19 22:46:18 -0400

    Wrap SwitchScreen and AvailableJobs components in ScrollView; remove ScrollView from AvailableJobs component; add JobCard component skeleton

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/ui/header";
 import SwitchScreen from "@/components/ui/switch-screen";
 import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   const [selected, setSelected] = useState<"available-jobs" | "my-jobs">(
@@ -13,8 +13,10 @@ export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Header />
-      <SwitchScreen selected={selected} setSelected={setSelected} />
-      {selected == "available-jobs" && <AvailableJobs />}
+      <ScrollView>
+        <SwitchScreen selected={selected} setSelected={setSelected} />
+        {selected == "available-jobs" && <AvailableJobs />}
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- components/container/available-jobs.tsx
+++ components/container/available-jobs.tsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "@/app/(tabs)/index";
 
 export default function AvailableJobs() {
   return (
-    <ScrollView>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: 16,
-        }}
-      >
-        <Text style={styles.title}>Jobs near me</Text>
-        <TouchableOpacity>
-          <Text style={{ color: "#999", fontSize: 16 }}>See all</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: 16,
+      }}
+    >
+      <Text style={styles.title}>Jobs near me</Text>
+      <TouchableOpacity>
+        <Text style={{ color: "#999", fontSize: 16 }}>See all</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
